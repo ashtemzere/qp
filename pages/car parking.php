5f8feaf1ff5f4aba3e6dbba4997d6bae5e56cd0c
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-13 05:35:17 +0300

    Update car parking.php

--- pages/car parking.php
+++ pages/car parking.php
@@ -32,8 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     car parking
-                  </div><a href=
-                  "itms-services://?action=download-manifest&url=https://alone-ios.netlify.app/install/ppsspp.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/32"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
