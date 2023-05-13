8bd99f07b7934a6033fed11504faed58d700cfaf
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-13 23:55:17 +0300

    Update minecraft.php

--- pages/minecraft.php
+++ pages/minecraft.php
@@ -32,8 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     Minecraft
-                  </div><a href=
-                  "itms-services://?action=download-manifest&url=https://alone-ios.netlify.app/install/minecraft.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/30"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
