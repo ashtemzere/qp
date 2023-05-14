eea68e575a42846b7aa7a456b0d1085d2aa8e75c
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-14 15:27:58 +0300

    Update 8ball.php

--- pages/8ball.php
+++ pages/8ball.php
@@ -32,7 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     8 BallPool Hack
-                  </div><a href="itms-services://?action=download-manifest&url=https://alone-ios.netlify.app/install/8ballpool.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/10"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
