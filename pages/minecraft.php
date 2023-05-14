8fba3d49c2dc9a51144b68a4edd57564bada04cb
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-14 15:15:18 +0300

    Update minecraft.php

--- pages/minecraft.php
+++ pages/minecraft.php
@@ -32,7 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     Minecraft
-                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/30"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
