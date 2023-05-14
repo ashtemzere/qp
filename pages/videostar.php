0c84826fa81935211572bdee173eb276abf27422
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-14 15:24:24 +0300

    Update videostar.php

--- pages/videostar.php
+++ pages/videostar.php
@@ -32,7 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     VideoStar pro
-                  </div><a href="location='itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/3"
+                  </div><a href='itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/3"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
