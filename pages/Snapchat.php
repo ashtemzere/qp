fb4254b46f8ba9c89579aa50dc3db5d864ba7d1a
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-15 15:47:11 +0300

    Add files via upload

---
+++ pages/Snapchat.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Snapchat ++</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+          <a href="#" class="link back"><span class=
+          "ios-only">Back</span></a>
+        </div>
+        <div class="title">
+          Snapchat ++
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <li class="media-item">
+            <div class="item-content">
+              <div class="item-media"><img src=
+              "./img/car parking.png"
+              class="appicon elevation-5 animated zoomIn"></div>
+              <div class="item-inner">
+                <div class="item-title-row">
+                  <div class="item-title">
+                    Snapchat ++
+                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/19"
+                  class=
+                  "button button-round button-fill button-raised color-green external">Get
+                  <i class=
+                  "icon f7-icons color-white">cloud_download</i></a>
+                </div>
+                <div class="item-subtitle">
+                  Hacked
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="block-title">
+        Snapchat ++
+      </div>
+      <div class="block block-strong inset elevation-12">
+        <pre>hack</pre>
+      </div>
+      <div class="list inset elevation-12">
+        <ul>
+          <li>
+            <div class="item-content">
+              <div class="item-inner">
+                <div class="item-title">
+                  Version
+                </div>
+                <div class="item-after">
+                  V12.19.0
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="item-content">
+              <div class="item-inner">
+                <div class="item-title">
+                  Size
+                </div>
+                <div class="item-after">
+                  84 MB
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
