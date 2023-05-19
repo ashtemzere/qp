350376bc271f7a109ffb7ba5f1d15f65b5d4d32e
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-20 00:33:56 +0300

    Update alone.php

--- pages/alone.php
+++ pages/alone.php
@@ -32,7 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     VideoStar pro
-                  </div><a href="itms-services://?action=download-manifest&url=https://app.kurdstore.info/plist/root.php/3"
+                  </div><a href="itms-services://?action=download-manifest&url=https://plist.tweakdoor.com/videostarold"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
