cb102f8e5391258dd776bffedaf99e800f6abeb1
ashtemzere <116907766+ashtemzere@users.noreply.github.com>
2023-05-19 15:54:57 +0300

    Add files via upload

---
+++ install/wallcraft.php
@@ -0,0 +1,47 @@
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd …">
+<plist version="1.0">
+<dict>
+<key>items</key>
+<array>
+<dict>
+<key>assets</key>
+<array>
+<dict>
+<key>kind</key>
+<string>software-package</string>
+<key>url</key>
+<string>https://archive.org/download/wallcraft-as-mobile/Wallcraft_as_mobile.ipa</string>
+</dict>
+<dict>
+<key>kind</key>
+<string>display-image</string>
+<key>url</key>
+<string>https://ashtemzere.ml/ashtemzere.png</string>
+</dict>
+<dict>
+<key>kind</key>
+<string>full-size-image</string>
+<key>url</key>
+<string>https://ashtemzere.ml/ashtemzere.png</string>
+</dict>
+</array>
+<key>metadata</key>
+<dict>
+<key>bundle-identifier</key>
+<string>as.mobile.wallpapers</string>
+<key>bundle-version</key>
+<string>83.09</string>
+<key>kind</key>
+<string>software</string>
+<key>title</key>
+<string>
+✔ ashte mzere ✔
+wallcraft
+</string>
+</dict> 
+</dict>
+</array>
+</dict>
+</plist>
